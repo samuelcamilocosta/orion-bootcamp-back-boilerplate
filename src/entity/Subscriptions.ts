b42b12f13855d1bf6ad8393800a13fa18871f22c
skrowll <lucasdejesus.rocha@hotmail.com>
2023-11-28 22:58:22 -0300

    feat: creates entity plans

--- src/entity/Subscriptions.ts
+++ src/entity/Subscriptions.ts
@@ -8,15 +8,15 @@ export class Subscription {
   @Column()
   user_id: number;
 
-  @Column({ type: 'enum', enum: ['monthly', 'semesterly', 'annually'] })
-  type: 'monthly' | 'semesterly' | 'annually';
-
-  @Column({ type: 'decimal', precision: 10, scale: 2 })
-  price: number;
+  @Column()
+  plan_id: number;
 
   @Column({ type: 'enum', enum: ['active', 'inactive'], default: 'active' })
   status: 'active' | 'inactive';
 
   @CreateDateColumn()
   started_at: Date;
+
+  @Column({ type: 'timestamp' })
+  ended_at: Date;
 }
