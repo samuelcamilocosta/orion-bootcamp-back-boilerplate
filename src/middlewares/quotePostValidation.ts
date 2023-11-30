9c9f1662e43b51a0dbf1f414342c469b58fa6296
skrowll <lucasdejesus.rocha@hotmail.com>
2023-11-30 20:21:18 -0300

    refactor: corrects misspelled word

--- src/middlewares/quotePostValidation.ts
+++ src/middlewares/quotePostValidation.ts
@@ -9,7 +9,7 @@ export class QuoteMiddleware {
       (req: Request, res: Response, next: NextFunction) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-          return res.status(400).json({ message: 'Os campos author e quote dever estar preenchidos com uma string' });
+          return res.status(400).json({ message: 'Os campos author e quote devem estar preenchidos com uma string' });
         }
         next();
       }
