b4da95511735a1d33e11cd5de295897da8b43c54
skrowll <lucasdejesus.rocha@hotmail.com>
2023-11-30 20:23:27 -0300

    feat: creates midlleware that validates body of req to creates a new plan

---
+++ src/middlewares/planPostValidation.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+
+export class PlanMiddleware {
+  public static validatePlan() {
+    return [
+      body('type').notEmpty().isIn(['monthly', 'semesterly', 'annually']),
+      body('price').notEmpty().isNumeric(),
+      body('saves_percentage').optional().isNumeric(),
+      body('description').optional().isString(),
+      (req: Request, res: Response, next: NextFunction) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ message: 'Todos os campos devem estar preenchidos corretamente' });
+        }
+        next();
+      }
+    ];
+  }
+}
