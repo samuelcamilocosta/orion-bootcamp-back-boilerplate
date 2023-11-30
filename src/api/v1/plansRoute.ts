411c4864a5a43617683b469da256d3477c1ffc3e
skrowll <lucasdejesus.rocha@hotmail.com>
2023-11-30 20:24:10 -0300

    feat: adds pos and delete routes in router

--- src/api/v1/plansRoute.ts
+++ src/api/v1/plansRoute.ts
@@ -2,11 +2,14 @@ import { Router } from 'express';
 import bodyParser from 'body-parser';
 import { PlanController } from './../../controller/PlanController';
 import { AuthMiddleware } from '../../middlewares/authValidation';
+import { PlanMiddleware } from '../../middlewares/planPostValidation';
 
 const router = Router();
 router.use(bodyParser.json());
 
 router.get('/plans', AuthMiddleware.authValidation, PlanController.getAllPlans);
 router.get('/plans/:id', AuthMiddleware.authValidation, PlanController.getPlanById);
+router.post('/plans', AuthMiddleware.authValidation, PlanMiddleware.validatePlan(), PlanController.createPlan);
+router.delete('/plans/:id', AuthMiddleware.authValidation, PlanController.deletePlanById);
 
 export default router;
