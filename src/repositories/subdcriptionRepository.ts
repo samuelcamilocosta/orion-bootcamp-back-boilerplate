2ef26d4d85fc701bc18351cdc490ced8686e2c1a
skrowll <lucasdejesus.rocha@hotmail.com>
2023-12-04 19:46:05 -0300

    feat: create subscriptionRepository

---
+++ src/repositories/subdcriptionRepository.ts
@@ -0,0 +1,29 @@
+import { MysqlDataSource } from '../config/database';
+import { Subscription } from './../entity/Subscriptions';
+
+/**
+ * Repository handling Subscription-related database queries.
+ */
+export class SubscriptionRepository {
+  /**
+   * Retrieves a subscription by user ID.
+   *
+   * @param userId - The ID of the user.
+   * @returns {Promise<Subscription | undefined>} A single Subscription object or undefined if not found.
+   */
+  public static async getSubscriptionByUserId(userId: number): Promise<Subscription | undefined> {
+    return MysqlDataSource.getRepository(Subscription).findOne({ where: { user_id: userId, status: 'active' } });
+  }
+
+  /**
+   * Creates a new subscription.
+   *
+   * @param newSubscription - The data for the new subscription.
+   * @returns {Promise<Subscription>} The created Subscription object.
+   */
+  public static async createSubscription(newSubscription: Partial<Subscription>): Promise<Subscription> {
+    const subscription = MysqlDataSource.getRepository(Subscription).create(newSubscription);
+
+    return MysqlDataSource.getRepository(Subscription).save(subscription);
+  }
+}
