603754a9d05a07ca14a19543efd6083a6a38b2f6
skrowll <lucasdejesus.rocha@hotmail.com>
2023-12-05 00:01:44 -0300

    Revert "refactor: changes column status to active and adds @AfterLoad to set status to active = false"
    This reverts commit 4d6d91a9a117efe7b7d4709714569d14be04d021.

--- src/entity/Subscriptions.ts
+++ src/entity/Subscriptions.ts
@@ -1,5 +1,4 @@
-import { MysqlDataSource } from '../config/database';
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, AfterLoad } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
 
 @Entity({ name: 'subscriptions' })
 export class Subscription {
@@ -12,31 +11,12 @@ export class Subscription {
   @Column()
   plan_id: number;
 
-  @Column({ type: 'boolean', default: true })
-  active: boolean;
+  @Column({ type: 'enum', enum: ['active', 'inactive'], default: 'active' })
+  status: 'active' | 'inactive';
 
   @CreateDateColumn()
   started_at: Date;
 
-  @Column({ type: 'timestamp', nullable: true })
+  @Column({ type: 'timestamp' })
   ended_at: Date;
-
-  @AfterLoad()
-  async setStatus(): Promise<void> {
-    const THIRTY_DAYS_IN_MS = 30 * 24 * 60 * 60 * 1000; // 30 dias em milissegundos
-    const currentDate = new Date();
-    const thirtyDaysAfterStart = new Date(this.started_at.getTime() + THIRTY_DAYS_IN_MS);
-    if (currentDate <= thirtyDaysAfterStart) {
-      this.active = true;
-    } else {
-      this.active = false;
-      if (this.ended_at === null) {
-        this.ended_at = new Date();
-      }
-    }
-    await MysqlDataSource.getRepository(Subscription).update(this.id, {
-      active: this.active,
-      ended_at: this.ended_at
-    });
-  }
 }
