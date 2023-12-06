836a40420fe3142201713d618093216aecc5a8af
skrowll <lucasdejesus.rocha@hotmail.com>
2023-12-05 23:21:30 -0300

    refactor: migration

--- src/migration/1701731316179-addSubscriptionForFabricio.ts
+++ src/migration/1701731316179-addSubscriptionForFabricio.ts
@@ -1,3 +1,4 @@
+import { Plan } from '../entity/Plans';
 import { Subscription } from '../entity/Subscriptions';
 import { User } from '../entity/Users';
 import { MigrationInterface, QueryRunner } from 'typeorm';
@@ -6,13 +7,15 @@ export class AddSubscriptionForFabricio1701731316179 implements MigrationInterfa
   public async up(queryRunner: QueryRunner): Promise<void> {
     const userRepository = queryRunner.manager.getRepository(User);
     const subscriptionRepository = queryRunner.manager.getRepository(Subscription);
+    const planRepository = queryRunner.manager.getRepository(Plan);
 
     const userToCreateSubscription = await userRepository.findOne({ where: { email: 'flindenm@hotmail.com' } });
+    const { id: plan_id } = await planRepository.findOne({ where: { type: 'monthly' } });
 
     if (userToCreateSubscription) {
       const newSubscription = subscriptionRepository.create({
         user_id: userToCreateSubscription.id,
-        plan_id: 1
+        plan_id
       });
 
       await subscriptionRepository.save(newSubscription);
@@ -22,14 +25,16 @@ export class AddSubscriptionForFabricio1701731316179 implements MigrationInterfa
   public async down(queryRunner: QueryRunner): Promise<void> {
     const userRepository = queryRunner.manager.getRepository(User);
     const subscriptionRepository = queryRunner.manager.getRepository(Subscription);
+    const planRepository = queryRunner.manager.getRepository(Plan);
 
     const userToRemoveSubscription = await userRepository.findOne({ where: { email: 'flindenm@hotmail.com' } });
+    const { id: plan_id } = await planRepository.findOne({ where: { type: 'monthly' } });
 
     if (userToRemoveSubscription) {
       const subscriptionToRemove = await subscriptionRepository.findOne({
         where: {
           user_id: userToRemoveSubscription.id,
-          plan_id: 1
+          plan_id
         }
       });
 
